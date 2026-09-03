67929c49712a594f2f95e5214778224b1c7bec21
RevTer <108422143+RevTer14@users.noreply.github.com>
2026-09-03 21:57:29 +0700

    Update touch_handler.h

--- cpp/gui/touch_handler.h
+++ cpp/gui/touch_handler.h
@@ -30,6 +30,8 @@ class touch_handler {
 
   bool handle(const touch_data& data)
   {
+    logger::log<logger::LV_SYSTEM, false>(nullptr, "[Touch RAW] type={} num={} x={} y={}", (int)data.type, data.num, data.x, data.y);
+    
     if (!touches_enabled || data.num >= IM_ARRAYSIZE(pointers)) {
       return false;
     }
